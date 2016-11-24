33693d24fb6030b0a9a0ffa31290e19fb95c0eff
elijahblack <elijah.black.23@gmail.com>
2016-11-24 10:51:56 +0600

    Добавлен метод просмотра параметров для класса Генератора ПСЧ

--- CesarCoder/PseudoRandomNumberGenerator.cs
+++ CesarCoder/PseudoRandomNumberGenerator.cs
@@ -12,11 +12,23 @@ namespace CesarCoder
         public int B;
         public int M;
 
+        /// <summary>
+        /// Инициализирует объект класса с заданными параметрами
+        /// </summary>
         public PseudoRandomNumberGenerator()
         {
             A = 3;
             B = 2;
             M = 1664525;
+        }
+
+        /// <summary>
+        /// Просмотр текущих параметров
+        /// </summary>
+        /// <returns>Возвращает значения трёх основных параметров объекта класса</returns>
+        public string GetParam()
+        {
+            return "A = " + A + ";   B = " + B + ";   M = " + M + ";";
         }
     }
 }
